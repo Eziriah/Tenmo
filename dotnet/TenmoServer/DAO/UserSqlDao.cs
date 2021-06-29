3d0d48330509e50e514003d12ed13ebe1da79296
Elysia Webb <elysia.n.webb@gmail.com>
2021-06-29 13:58:32 -0400

    first edits

--- dotnet/TenmoServer/DAO/UserSqlDao.cs
+++ dotnet/TenmoServer/DAO/UserSqlDao.cs
@@ -119,5 +119,30 @@ namespace TenmoServer.DAO
 
             return u;
         }
+
+        //step 3) create method to display balance using SQL
+        //might need to create an AccountDAO / interface
+        public decimal GetBalance(string username)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT @balance FROM accounts a JOIN users u ON u.user_id = a.user_id WHERE @username = username", conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    decimal balance = Convert.ToDecimal(cmd.ExecuteScalar());
+                    return balance;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
     }
 }
